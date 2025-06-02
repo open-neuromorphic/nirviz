806a736736b24dedf4864b1bd700be4bffb811a2
Michail Rontionov <m.rontionov@soton.ac.uk>
2025-06-02 18:55:17 +0100

    Add styling options based on style.yml
    The visualise dataclass can now style each node based on its name
    matched with pattern p in style.yml.
    
    The pattern gets transformed to regex "^p.*", which should capture as
    much as possible.
    
    More exhaustive understanding of what kind of names different
    simulators give should be considered.

--- nirviz.py
+++ nirviz.py
@@ -1,4 +1,6 @@
 import dataclasses
+import yaml
+import re
 import graphviz
 import cairosvg
 import nir
@@ -6,28 +8,58 @@ import typing
 import importlib.util
 import pathlib
 
+
 @dataclasses.dataclass
 class visualize:
-
     nir_graph: typing.Union[nir.NIRGraph, str, pathlib.Path]
     format: str = "svg"
     render_ipython: bool = True
+    style_file = pathlib.Path("./style.yml")
 
     def __post_init__(self):
         if isinstance(self.nir_graph, (str, pathlib.Path)):
             self.nir_graph = nir.read(self.nir_graph)
 
-        self.digraph = graphviz.Digraph(format=self.format)
+        self.style_dict = self.__load_style_file()
+        self.viz_graph = self.__construct_graph()
+
+    def __load_style_file(self) -> typing.Dict[str, typing.Dict[str, str]]:
+        with open(self.style_file, 'r') as f:
+            config = yaml.safe_load(f)
+        return config
+
+    def __pick_style(self, name: str) -> typing.Dict[str, str]:
+        categories = self.style_dict['node-categories']
+        for cat in categories:
+            for p in cat['patterns']:
+                regexp = re.compile(f"^{p}.*")
+                if re.fullmatch(regexp, name.lower()):
+                    return cat['style']
+        return categories['defaults']['node']
+
+
+    def __construct_graph(self) -> graphviz.Digraph:
+        viz_graph = graphviz.Digraph(format=self.format,
+                                     graph_attr={'rankdir': 'LR'})
+
+        # Generate nodes
         for name in self.nir_graph.nodes.keys():
-            self.digraph.node(name)
+            style = self.__pick_style(name)
+            viz_graph.node(name, **style)
+
+        # Generate edges
         for src, tgt in self.nir_graph.edges:
-            self.digraph.edge(src, tgt)
+            viz_graph.edge(src, tgt)
+
+        return viz_graph
+
+
 
     def __repr__(self) -> str:
         if self.render_ipython and not importlib.util.find_spec("IPython") is None:
             import IPython
 
-            svg_output = self.digraph.pipe(format="svg")
+            svg_output = self.viz_graph.pipe(format="svg")
 
             if self.format == "svg":
                 image = IPython.display.SVG(svg_output)
@@ -39,4 +71,4 @@ class visualize:
                 raise ValueError(f"Unsupported format: {self.format}")
             IPython.display.display(image)
 
-        return self.digraph.pipe(format=self.format, encoding="utf-8")
+        return self.viz_graph.pipe(format=self.format, encoding="utf-8")
