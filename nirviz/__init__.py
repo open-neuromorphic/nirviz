1bfbdf92201c446b445c50cc83fd6e27f8c449b2
Michail Rontionov <m.rontionov@soton.ac.uk>
2025-06-08 11:09:33 +0100

    Changed package hierarchy for less clutter

--- nirviz/__init__.py
+++ nirviz/__init__.py
@@ -1,8 +1,12 @@
 from importlib.metadata import version as metadata_version, PackageNotFoundError
 
+# Exposing imports
+from .nirviz import visualize
+
 try:
+    # For pyproject.toml dynamic versioning
     __version__ = version = metadata_version("nirviz")
     del metadata_version
 except PackageNotFoundError:
-    # package is not installed
+    print('nirviz: package not found')
     pass
